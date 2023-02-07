81c968897e31553c5cfba198b7e779758db6f599
bambee83 <daj0909@naver.com>
2023-02-07 14:58:41 +0900

    app.py add music

--- app.py
+++ app.py
@@ -36,7 +36,28 @@ for musical in musicals:
         # }
         # db.culture.insert_one(doc)  #데이터 삽입.
 
+headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+data = requests.get('https://www.genie.co.kr/chart/top200?ditc=M&rtm=N&ymd=20210701',headers=headers)
 
+soup = BeautifulSoup(data.text, 'html.parser')
+
+genies = soup.select('#body-content > div.newest-list > div > table > tbody > tr')
+for genie in genies :
+    music = genie.select_one('td.number').text[0:2]
+    image = genie.select_one('td:nth-child(3) > a > img')['src']
+    name = genie.select_one('td.info > a.title.ellipsis').text.strip()
+    date = genie.select_one('td.info > a.albumtitle.ellipsis').text
+    print(music,image, name, date)
+
+
+    doc = {
+    'category':'music',
+    'image':'image',
+    'name':'name',
+    'desc':'desc',
+    'date':'date',
+    }
+    db.culture.insert_one(doc)
 
 # 송 수신
 @app.route('/')
